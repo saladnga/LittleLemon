29745e5edbd7adc67da2b989f839c84112a38ae5
Vu Hoang <vulego2004@gmail.com>
2024-11-26 16:44:26 -0600

    add models and admin

--- littlelemonproject/restaurant/models.py
+++ littlelemonproject/restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    number_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"{self.name} for {self.number_of_guests} guests on {self.booking_date}"
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.title} : {str(self.price)}"
